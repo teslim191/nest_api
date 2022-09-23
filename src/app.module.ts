fa74594d5b53cb1f9853ab2d295cf3f06edecddf
unknown <teslimjimoh191@gmail.com>
2022-09-23 21:19:20 +0100

    added a .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import config from './config/keys';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ItemsModule, MongooseModule.forRoot(config.MONGO_URI)],
+  imports: [
+    ConfigModule.forRoot(),
+    ItemsModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/keys.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  MONGO_URI: `mongodb+srv://teslim:youngmoney911@cluster0.evl07.mongodb.net/?retryWrites=true&w=majority`,
-};
